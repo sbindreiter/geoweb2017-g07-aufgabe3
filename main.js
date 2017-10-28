c0d081099808973c6c33643a6a8c63786a2cea48
sbindreiter <33084146+sbindreiter@users.noreply.github.com>
2017-10-28 14:01:04 +0200

    block 3 learning git hub

---
+++ main.js
@@ -0,0 +1,47 @@
+import 'ol/ol.css';
+import View from 'ol/view';
+import Map from 'ol/map';
+import TileLayer from 'ol/layer/tile';
+import Stamen from 'ol/source/stamen';
+import VectorLayer from 'ol/layer/vector';
+import Vector from 'ol/source/vector';
+import GeoJSON from 'ol/format/geojson';
+import Style from 'ol/style/style';
+import Text from 'ol/style/text';
+import Stroke from 'ol/style/stroke';
+import proj from 'ol/proj';
+
+//alert('Hello Workshop');
+const map = new Map({
+  target: 'map',
+  view: new View({
+    center: proj.fromLonLat([16.37, 48.2]),
+    zoom: 13
+  })
+});
+map.addLayer(new TileLayer({
+  source: new Stamen({
+    layer: 'watercolor'
+  })
+}));
+
+const layer = new VectorLayer({
+  source: new Vector({
+    url: 'data/map.geojson',
+    format: new GeoJSON()
+  })
+});
+map.addLayer(layer);
+
+layer.setStyle(function(feature) {
+  return new Style({
+    text: new Text({
+      text: feature.get('Name'),
+      font: 'Bold 14pt Verdana',
+      stroke: new Stroke({
+        color: 'white',
+        width: 3
+      })
+    })
+  });
+});
